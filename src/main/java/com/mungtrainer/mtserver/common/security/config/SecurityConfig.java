7b8e10c406fdc374cc01ce8f71ee4cc9ec269265
YejinKang <ks990528@naver.com>
2025-12-16 00:19:37 +0900

    ci test15

--- src/main/java/com/mungtrainer/mtserver/common/security/config/SecurityConfig.java
+++ src/main/java/com/mungtrainer/mtserver/common/security/config/SecurityConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Profile("prod")
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
