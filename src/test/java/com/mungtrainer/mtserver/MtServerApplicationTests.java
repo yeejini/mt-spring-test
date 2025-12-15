dd53436e3e64f512e36669473cfdf98d1a97a2f3
YejinKang <ks990528@naver.com>
2025-12-15 22:48:17 +0900

    ci test7

--- src/test/java/com/mungtrainer/mtserver/MtServerApplicationTests.java
+++ src/test/java/com/mungtrainer/mtserver/MtServerApplicationTests.java
@@ -1,11 +1,14 @@
 package com.mungtrainer.mtserver;
 
+import com.mungtrainer.mtserver.config.AwsS3TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(AwsS3TestConfig.class)
 class MtServerApplicationTests {
 
 	@Test
