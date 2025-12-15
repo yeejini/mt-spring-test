682f65624bd496b4ebfef1b25534d6d341211805
YejinKang <ks990528@naver.com>
2025-12-15 23:36:24 +0900

    ci test11

--- src/test/java/com/mungtrainer/mtserver/config/AwsS3TestConfig.java
+++ src/test/java/com/mungtrainer/mtserver/config/AwsS3TestConfig.java
@@ -3,6 +3,7 @@ package com.mungtrainer.mtserver.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import static org.mockito.Mockito.mock;
 
@@ -11,7 +12,11 @@ public class AwsS3TestConfig {
 
     @Bean
     public S3Client s3Client() {
-        // 실제 AWS 호출 없이 Mock 객체 사용
         return mock(S3Client.class);
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return mock(S3Presigner.class);
     }
 }
